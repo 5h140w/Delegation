a8ec70c0f3f13d82f0e9b18aa1db54d4c8c2b99d
5h140w <5h140w23@gmail.com>
2022-06-16 14:32:01 +0100

    navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,29 +2,32 @@ import React , {useEffect , useState} from 'react'
 import "./Navbar.css"
 
 const Navbar = () =>{
-
     const [isScrolled, setIsScrolled] = useState(false)
-
+    const [isOpen, setisOpen] = useState(false)
     window.onscroll = () =>{
         setIsScrolled(window.pageYOffset !== 0)
     }
+    const handleClick = () =>{
+        setisOpen(!isOpen)
+    }
+
     return (
         <>
             <nav className={isScrolled ? "navbar scrolled" : "navbar"}>
                 <section className="navbar_container">
                     <div className="logo_section">
                         <h1>Lena Instruments</h1>
-                        <button>
+                        <button onClick ={handleClick}>
                             <i className="fas fa-bars"></i>             
                         </button>
                     </div>
-                    <ul className="menu_navbar">
+                    <ul className={isOpen ? "menu_navbar" : "none"}>
                         <li>Home</li>
                         <li>About</li>
                         <li>Contact</li>
                         <li>Delegate</li>
                     </ul>
-                    <button>Join Now</button>
+                    <button className={isOpen ? "" : "none"}>Join Now</button>
                 </section>
             </nav>
         </>
