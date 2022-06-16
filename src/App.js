8ffb4c857c98481bfc01518a667cd40193f80d9b
5h140w <5h140w23@gmail.com>
2022-06-16 13:40:14 +0100

    navbar

--- src/App.js
+++ src/App.js
@@ -1,57 +1,9 @@
-import styled, { css } from "styled-components";
-import Contact from "./components/Contact";
-import Feature from "./components/Feature";
-import Footer from "./components/Footer";
-import Intro from "./components/Intro";
-import Navbar from "./components/Navbar";
-const Container = styled.div`
-  height: 100vh;
-  overflow: hidden;
-  position: relative;
-`;
-
-const Shape = css`
-  width: 100%;
-  height: 100%;
-  position: absolute;
-  top: 0;
-  left: 0;
-  z-index: -1;
-`;
-
-const IntoShape = styled.div`
-  ${Shape}
-  clip-path: polygon(67% 0, 100% 0%, 100% 100%, 50% 100%);
-  background-color: #f9fcfd;
-`;
-
-const FeatureShape = styled.div`
-  ${Shape}
-  clip-path: polygon(0 0, 50% 0%, 33% 100%, 0 100%);
-  background-color: #f6f6f6;
-
-
-`;
-
-
-
+import Navbar from "./components/Navbar/Navbar";
 
 const App = () => {
   return (
     <>
-      <Container>
-        <Navbar />
-        <Intro />
-        <IntoShape />
-      </Container>
-      <Container>
-        <Feature />
-        <FeatureShape />
-      </Container>
-      <Container>
-        <Contact />
-        <Footer />
-      </Container>
+      <Navbar></Navbar>
     </>
   );
 };
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,8 +1,27 @@
 import React from 'react'
+import "./Navbar.css"
 
 const Navbar = () =>{
     return (
-        <></>
+        <>
+            <nav className="navbar">
+                <section className="navbar_container">
+                    <div className="logo_section">
+                        <h1>Lena Instruments</h1>
+                        <button>
+                            <i className="fas fa-bars"></i>             
+                        </button>
+                    </div>
+                    <ul>
+                        <li>Home</li>
+                        <li>About</li>
+                        <li>Contact</li>
+                        <li>Delegate</li>
+                    </ul>
+                    <button>Join Now</button>
+                </section>
+            </nav>
+        </>
     )
 }
 
