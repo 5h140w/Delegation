4f3125c75af9db2c4a4277e5e3260d11370b8e4f
5h140w <5h140w23@gmail.com>
2022-06-18 12:16:12 +0100

    footer content

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+
+const Footer = () =>{
+    return(
+        <div className='footer_container'>
+            <div className='footer_wrapper'>
+                <ul className='footer_menu'>
+                    <li>Guide</li>
+                    <li>Support</li>
+                    <li>API</li>
+                    <li>Community</li>
+                </ul>
+                <span className='footer_copyright'>Lena Instruments Ⓒ</span>
+            </div>
+        </div>
+    )
+}
+
+export default Footer
